52875ac856e2149e419c9c440df26891ce02f6ab
thak1411 <thak1411@gmail.com>
2021-10-09 01:30:41 +0900

    Update: cmd main

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,31 @@
 package main
 
+import (
+	"fmt"
+
+	rnjson "github.com/thak1411/rn-json"
+)
+
 func main() {
-	// var result map[string]interface{}
-	// dat := `
-	// {
-	// 	"foo": "bar",
-	// 	"foo2": "bar2",
-	// 	"foo3": {
-	// 		"foo4": "foo5",
-	// 		"foo5": {
-	// 			"foo6": "foo7"
-	// 		}
-	// 	}
-	// }
-	// `
-	// json.Unmarshal([]byte(dat), &result)
-	// fmt.Println(JsonGet(result, "foo3.foo5"))
-	// fmt.Println(JsonGet(result, "foo3.foo5.foo7"))
-	// fmt.Println(JsonGet(result, "foo3.foo5.foo8"))
-	// fmt.Println(JsonGet(result, "foo3.foo5.foo6"))
-	// fmt.Println(JsonGet(result, "foo3.foo5.foo6.foo8"))
-	// fmt.Println(JsonGet(result, "foo3.foo5.foo6.foo8.foo9"))
+	dat := `
+	{
+		"foo": "bar",
+		"foo2": "bar2",
+		"foo3": {
+			"foo4": "bar4",
+			"foo5": {
+				"foo6": "bar6"
+			}
+		}
+	}
+	`
+	result, err := rnjson.Unmarshal(dat)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rnjson.Get(result, "foo"))
+	fmt.Println(rnjson.Get(result, "foo3.foo5"))
+	fmt.Println(rnjson.Get(result, "foo3.foo5.foo6"))
+	fmt.Println(rnjson.Get(result, "foo3.foo5.foo6.foo8"))
+	fmt.Println(rnjson.Get(result, "foo3.foo5.foo6.foo8.foo9"))
 }
